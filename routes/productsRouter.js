a2e1cfdfe9022d0d6318140f8b71aa5e7aca1bb0
Javier Ospino <javierospinoac@unimagdalena.edu.co>
2024-03-05 19:07:10 -0500

    Actualizar rutas para usar funciones asincrónicas en ProductService

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,30 +5,46 @@ const ProductService = require('../services/productService');
 const router = express.Router();
 const service = new ProductService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
-  res.status(200).json(products);
+router.get('/', async (req, res) => {
+  try {
+    const products = await service.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.get('/filter', (req, res) => {
+/*router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
+});*/
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const product = service.findOne(id);
-  res.status(200).json(product);
+router.post('/', async (req, res) => {
+  try {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
-});
-
-router.put('/:id', (req, res) => {
+/*router.put('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
   res.json({
@@ -36,25 +52,31 @@ router.put('/:id', (req, res) => {
     data: body,
     id
   });
+});*/
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'updated',
-    data: body,
-    id
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'deleted',
-    id
-  });
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const del = await service.delete(id);
+    res.status(200).json(del);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -7,7 +7,7 @@ class ProductService {
     this.generate();
   }
 
-  generate(){
+  async generate(){
     const limit = 100;
 
     for (let i = 0; i < limit; i++) {
@@ -20,24 +20,50 @@ class ProductService {
     }
   }
 
-  create(){
-
+  async create(data){
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
-  find(){
+  async find(){
+    if(this.products.length < 1){
+      throw new Error("Products not found");
+    }
     return this.products;
   }
 
-  findOne(id){
+  async findOne(id){
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error("Product not found");
+    }
     return this.products.find(item => item.id === id);
   }
 
-  update(){
-
+  async update(id, changes){
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error("Product not found");
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
   }
 
-  delete(){
-
+  async delete(id){
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error("Product not found");
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 }
 
